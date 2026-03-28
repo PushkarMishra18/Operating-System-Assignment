0cc842167c08875728a8a355a25ede4f53caa5c1
PushkarMishra18 <pushkarmishra259@gmail.com>
2026-03-28 15:20:44 +0530

    Add FCFS scheduling algorithm implementation

---
+++ Assignment1/FCFS.py
@@ -0,0 +1,26 @@
+# Input
+n = int(input("Enter number of processes: "))
+p = []
+
+for i in range(n):
+    arrival_time = int(input(f"Arrival Time of P{i}: "))
+    burst_time = int(input(f"Burst Time of P{i}: "))
+    p.append([i, arrival_time, burst_time])
+
+# FCFS
+p.sort(key=lambda x: x[1])
+time = 0
+result = []
+
+for i in p:
+    time = max(time, i[1]) + i[2]
+    completion_time = time
+    turnaround_time = completion_time - i[1]
+    waiting_time = turnaround_time - i[2]
+    result.append([i[0], i[1], i[2], completion_time, turnaround_time, waiting_time])
+
+# Output
+print("\nFCFS Table:")
+print("PID\tAT\tBT\tCT\tTAT\tWT")
+for row in result:
+    print(*row, sep="\t")
